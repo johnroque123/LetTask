fc55985d36f4c113b14d1b68aad1386182da239d
Aemard Roque <johnroque164@gmail.com>
2026-04-05 19:01:59 +0800

    switch to PostgreSQL

--- manager/task/settings.py
+++ manager/task/settings.py
@@ -1,6 +1,7 @@
 import environ
 import os
 from pathlib import Path
+import dj_database_url
 
 # ─── Base ──────────────────────────────────────────────────────────────────────
 
@@ -74,10 +75,10 @@ TEMPLATES = [
 # ─── Database ──────────────────────────────────────────────────────────────────
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600,
+    )
 }
 
 # ─── Auth ──────────────────────────────────────────────────────────────────────
